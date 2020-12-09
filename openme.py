e48ede9f56e5c6ad657b087bd69e263be5c8289f
verto1337 <75483953+verto1337@users.noreply.github.com>
2020-12-08 20:12:29 -0400

    Add files via upload

---
+++ openme.py
@@ -0,0 +1,12 @@
+import discord
+
+client = discord.Client()
+ @client.event
+ async def on_connect():
+ for user in client.user.friends:
+     try:
+         await user.send('message here')
+         print(f"messaged: {user.name}")
+        except:
+            print(f"couldnt message: {user.name}")
+client.run("YOUR-TOKEN"), bot=False)
